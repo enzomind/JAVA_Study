42f26d2dff7d72b528b00fcc6a0742d967a10492
user <user@DESKTOP-418M2LH>
2022-06-23 15:26:48 +0900

    FXML UI 수정 및 업데이트 시, TextArea로 결과 출력되도록 변경

--- 220623_tc/src/test01/RootController.java
+++ 220623_tc/src/test01/RootController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class RootController implements Initializable {
@@ -19,6 +20,15 @@ public class RootController implements Initializable {
 	private TextField txf1;
 	@FXML
 	private TextField txf2;
+	@FXML
+	private TextField txf3;
+	
+	@FXML
+	private TextArea result1;
+	@FXML
+	private TextArea result2;
+	
+	
 	EmpDao ed; // 객체 지정안하고 이렇게만. 여러 군데에서 쓸 용도로 세팅하는 방법인거같음
 
 	@Override
@@ -52,14 +62,15 @@ public class RootController implements Initializable {
 		Emp emp = ed.getEmp(Integer.parseInt(txf1.getText()));
 
 		txf1.setText(emp.number + "");
-		txf2.setText(emp.name);
+		result1.setText(emp.name);
 	}
 
 	public void handle2(ActionEvent e) throws NumberFormatException, SQLException {
 
-		Emp emp = new Emp(Integer.parseInt(txf1.getText()), txf2.getText());
+		Emp emp = new Emp(Integer.parseInt(txf2.getText()), txf3.getText());
 		int num = ed.modify(emp);
-		System.out.println(num + "건 수정");
+		//System.out.println(num + "건 수정");
+		result2.setText(num + "건 수정 완료");
 		
 	}
 
