4881f23036bd0ebfce6036c765b53359332311d7
Logan <redkal19@gmail.com>
2022-06-25 16:37:54 +0900

    Merge branch 'main' of https://github.com/enzomind/JAVA_Study.git into main

--- ysproject/src/teampj/DBConn.java
+++ ysproject/src/teampj/DBConn.java
@@ -7,8 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import teamProject.School;
-
 public class DBConn {
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  // mysql
 	static final String DB_URL = "jdbc:mysql://localhost:3306/teampj_db?useSSL=false"; 
