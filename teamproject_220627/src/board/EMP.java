69f7b6c4a19ff0c6df528a81e6809ec7716f7090
Logan <102308415+enzomind@users.noreply.github.com>
2022-06-27 17:18:14 +0900

    Update EMP.java

--- teamproject_220627/src/board/EMP.java
+++ teamproject_220627/src/board/EMP.java
@@ -8,12 +8,13 @@ import javafx.beans.property.StringProperty;
 public class EMP {
 
 	
-	private StringProperty mid;
-	private StringProperty pw;
+	private StringProperty mid; //아이디
+	private StringProperty pw;  //패스워드
 	
-	private IntegerProperty bno;
-	private StringProperty title;
-	private StringProperty contents;
+	private IntegerProperty btype //게시글 유형 (1:공지사항 / 2:FAQ / 3:자유게시판)
+	private IntegerProperty bno; //게시글번호
+	private StringProperty title; //게시글 타이틀
+	private StringProperty contents; //게시글 내용
 	
 	
 }
