8b7b4e09306de38e1e4381f374634579d6267a74
Logan <102308415+enzomind@users.noreply.github.com>
2022-06-27 17:31:12 +0900

    v0.2 btype 필드 추가 (게시판 유형) 및 주석 추가
    게시글 유형에 따른 필드 추가

--- teamproject_220627/src/board/EMP.java
+++ teamproject_220627/src/board/EMP.java
@@ -11,7 +11,7 @@ public class EMP {
 	private StringProperty mid; //아이디
 	private StringProperty pw;  //패스워드
 	
-	private IntegerProperty btype //게시글 유형 (1:공지사항 / 2:FAQ / 3:자유게시판)
+	private IntegerProperty btype; //게시글 유형 (1:공지사항 / 2:FAQ / 3:자유게시판)
 	private IntegerProperty bno; //게시글번호
 	private StringProperty title; //게시글 타이틀
 	private StringProperty contents; //게시글 내용
